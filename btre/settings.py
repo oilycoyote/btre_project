aefe9ff3b91acb9ccaf35c2952dcb7101a028433
oilycoyote <juan.aguayo@aguayo.co>
2020-04-11 12:15:04 -0500

    Settings changed

--- btre/settings.py
+++ btre/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'AguayoUX',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
